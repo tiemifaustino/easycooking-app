d4c40d1856f1664f79e9a6dd6c017eb33c5fb9dd
Tamiris Shigaki <shigakiendo@gmail.com>
2022-04-26 20:58:12 -0300

    :heavy_check_mark: feat: requisito 13

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
+import { reduce } from 'lodash';
 
 test('Farewell, front-end', () => {
   const { getByText } = render(<App />);
