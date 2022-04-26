cff9c719923277a4d7d9ad74870353b79f6fb666
Tiemi Faustino <hayashipharma@gmail.com>
2022-04-26 17:45:19 -0300

    heavy_check_mark feat: requisito 1, 2, 3, 4 e 5

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,10 +1,59 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 function Login() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [isLoginBtnDisabled, setIsLoginBtnDisabled] = useState(true);
+
+  function handleChange(event, setState) {
+    setState(event.value);
+  }
+
+  useEffect(() => {
+    const passwordMinLength = 6;
+
+    const passwordValid = password.length > passwordMinLength;
+
+    const regexEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    const mailValidator = regexEmail.test(email);
+
+    const isValid = (passwordValid && mailValidator);
+    if (!isValid) {
+      setIsLoginBtnDisabled(true);
+    } else {
+      setIsLoginBtnDisabled(false);
+    }
+  }, [email, password]);
+
   return (
-    <h1>
-      Teste
-    </h1>
+    <>
+      <h1>Login</h1>
+      <form>
+        <input
+          data-testid="email-input"
+          type="email"
+          value={ email }
+          onChange={ (event) => handleChange(event.target, setEmail) }
+          placeholder="Email"
+        />
+
+        <input
+          data-testid="password-input"
+          type="password"
+          value={ password }
+          onChange={ (event) => handleChange(event.target, setPassword) }
+          placeholder="Password"
+        />
+
+        <button
+          data-testid="login-submit-btn"
+          disabled={ isLoginBtnDisabled }
+          type="submit"
+        >
+          Enter
+        </button>
+      </form>
+    </>
   );
 }
 
