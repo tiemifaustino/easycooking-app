8e89236c6c4bb8a724938308f12b3227a1979a62
Hugorc10 <hugorc10@hotmail.com>
2022-04-26 18:38:38 -0300

    :heavy_check_mark: feat: requisito 11 e 12

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { string } from 'prop-types';
+import { useHistory } from 'react-router-dom';
 import profileIcon from '../images/profileIcon.svg';
 import searchIcon from '../images/searchIcon.svg';
 
 function Header(props) {
   const { title, visible } = props;
+  const history = useHistory();
+  const [showInput, setShowInput] = useState(false);
+
+  const handleProfileClick = () => {
+    history.push('/profile');
+  };
+
+  const handleSearchClick = () => {
+    setShowInput(!showInput);
+  };
+
   return (
     <header>
-      <button type="button">
+      <button type="button" onClick={ handleProfileClick }>
         <img src={ profileIcon } alt="profileIcon" data-testid="profile-top-btn" />
       </button>
       <h1 data-testid="page-title">
@@ -15,9 +27,14 @@ function Header(props) {
       </h1>
       {
         visible && (
-          <button type="button">
+          <button type="button" onClick={ handleSearchClick }>
             <img src={ searchIcon } alt="searchIcon" data-testid="search-top-btn" />
           </button>
+        )
+      }
+      {
+        showInput && (
+          <input type="text" placeholder="Search" data-testid="search-input" />
         )
       }
 
