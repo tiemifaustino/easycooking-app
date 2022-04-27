c1484f99dd8b175b36f7f80c6b01f491bc635a56
Tamiris Shigaki <shigakiendo@gmail.com>
2022-04-27 00:34:27 -0300

    :construction: fix: erro no requisito 15

---
+++ src/reducers/cocktailReducer.js
@@ -0,0 +1,29 @@
+import { REQUEST_COCKTAIL,
+  REQUEST_COCKTAIL_SUCCESS,
+  REQUEST_COCKTAIL_FAILURE } from '../actions/index.actions';
+
+const INICIAL_STATE = {
+  cocktail: [],
+  error: '',
+};
+
+const cocktailReducer = (state = INICIAL_STATE, action) => {
+  switch (action.type) {
+  case REQUEST_COCKTAIL:
+    return { ...state };
+  case REQUEST_COCKTAIL_SUCCESS:
+    return {
+      ...state,
+      recipe: action.recipe,
+    };
+  case REQUEST_COCKTAIL_FAILURE:
+    return {
+      ...state,
+      error: action.error,
+    };
+  default:
+    return state;
+  }
+};
+
+export default cocktailReducer;
--- src/services/API.js
+++ src/services/API.js
@@ -1,4 +1,4 @@
-async function fetchRecipe(searchValue) {
+export async function fetchRecipe(searchValue) {
   let URL = '';
   if (searchValue.typeInput === 'Ingredient') URL = `https://www.themealdb.com/api/json/v1/1/filter.php?i=${searchValue.search}`;
   if (searchValue.typeInput === 'Name') URL = `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchValue.search}`;
@@ -9,4 +9,13 @@ async function fetchRecipe(searchValue) {
   return response.ok ? Promise.resolve(json) : Promise.reject(json);
 }
 
-export default fetchRecipe;
+export async function fetchCocktail(searchValue) {
+  let URL = '';
+  if (searchValue.typeInput === 'Ingredient') URL = `https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=${searchValue.search}`;
+  if (searchValue.typeInput === 'Name') URL = `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${searchValue.search}`;
+  if (searchValue.typeInput === 'First Letter') URL = `https://www.themealdb.com/api/json/v1/1/search.php?f=${searchValue.search}`;
+  const response = await fetch(URL);
+  const json = await response.json();
+
+  return response.ok ? Promise.resolve(json) : Promise.reject(json);
+}
