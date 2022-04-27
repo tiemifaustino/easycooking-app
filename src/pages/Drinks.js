0add242a97673f214c1b0e834d7cf9ac36e6bab9
Tamiris Shigaki <shigakiendo@gmail.com>
2022-04-27 18:19:30 -0300

    :heavy_check_mark: feat: requisito 17

--- src/pages/Drinks.js
+++ src/pages/Drinks.js
@@ -1,9 +1,28 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import Header from '../components/Header';
+import Cards from '../components/Cards';
 
 function Drinks() {
+  const drinks = useSelector((state) => state.cocktailReducer.cocktail.drinks);
   return (
-    <Header title="Drinks" visible />
+    <div>
+      <Header title="Drinks" visible />
+      {
+        drinks && drinks.map((drink, index) => {
+          const maxdrinks = 11;
+          if (index > maxdrinks) return;
+          return (
+            <Cards
+              key={ drink.strDrink }
+              img={ drink.strDrinkThumb }
+              index={ index }
+              title={ drink.strDrink }
+            />
+          );
+        })
+      }
+    </div>
   );
 }
 
--- src/pages/Foods.js
+++ src/pages/Foods.js
@@ -1,9 +1,29 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import Header from '../components/Header';
+import Cards from '../components/Cards';
 
 function Foods() {
+  const foods = useSelector((state) => state.recipeReducer.recipe.meals);
   return (
-    <Header title="Foods" visible />
+    <div>
+      <Header title="Foods" visible />
+
+      {
+        foods && foods.map((meal, index) => {
+          const maxFoods = 11;
+          if (index > maxFoods) return;
+          return (
+            <Cards
+              key={ meal.strMeal }
+              img={ meal.strMealThumb }
+              index={ index }
+              title={ meal.strMeal }
+            />
+          );
+        })
+      }
+    </div>
   );
 }
 
