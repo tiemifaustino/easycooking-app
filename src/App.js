cb0e9346e48f24d3446bafff153a29a0a20fc106
Tiemi Faustino <hayashipharma@gmail.com>
2022-04-27 16:40:03 -0300

    :construction: tela receita em progresso em andamento

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
-import Login from './pages/Login';
-import Foods from './pages/Foods';
-import Explore from './pages/Explore';
-import Drinks from './pages/Drinks';
 import DoneRecipes from './pages/DoneRecipes';
+import Drinks from './pages/Drinks';
+import DrinksInProgress from './pages/DrinksInProgress';
+import Explore from './pages/Explore';
 import ExploreDrinks from './pages/ExploreDrinks';
 import ExploreFoods from './pages/ExploreFoods';
 import ExploreIngredients from './pages/ExploreIngredients';
 import ExploreNationalities from './pages/ExploreNationalities';
-import Profile from './pages/Profile';
 import FavoriteRecipes from './pages/FavoriteRecipes';
+import Foods from './pages/Foods';
+import Login from './pages/Login';
+import Profile from './pages/Profile';
 
 function App() {
   return (
@@ -28,7 +29,7 @@ function App() {
       <Route exact path="/explore/drinks" component={ ExploreDrinks } />
       <Route exact path="/explore/foods" component={ ExploreFoods } />
       <Route exact path="/explore" component={ Explore } />
-      <Route exact path="/drinks/:id/in-progress" />
+      <Route exact path="/drinks/:id/in-progress" component={ DrinksInProgress } />
       <Route exact path="/foods/:id/in-progress" />
       <Route exact path="/drinks/:id" />
       <Route exact path="/foods/:id" />
