d929d05b91c54efa87561a9833bbb36a9149e951
Gabriel Pesch <gkpesch@gmail.com>
2022-04-27 22:45:06 -0300

    :heavy_check_mark: feat: req 33 e 34

---
+++ src/pages/RecipeDetails.js
@@ -0,0 +1,60 @@
+import React, { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { useParams } from 'react-router-dom';
+import { requestRecipeByIDThunk } from '../actions/index.actions';
+
+function RecipeDetails() {
+  const { id } = useParams();
+  const dispatch = useDispatch();
+  // const { recipe } = useSelector((state) => state.recipeReducer);
+
+  useEffect(() => {
+    dispatch(requestRecipeByIDThunk(id));
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  return (
+    <div>
+      <img src="" alt="foto da receita x" data-testid="recipe-photo" />
+      <div>
+        <h2 data-testid="recipe-title">Titulo da Receita</h2>
+        <button type="button" data-testid="share-btn">Share Btn</button>
+        <button type="button" data-testid="favorite-btn">Favoritar</button>
+        <p data-testid="recipe-category">Tipo de receita</p>
+      </div>
+
+      <div>
+        <ul>
+          <h2>Ingredients</h2>
+          <li
+            data-testid={ `${0}-ingredient-name-and-measure` }
+          >
+            primeiro ingrediente
+
+          </li>
+          <li>segundo ingrediente</li>
+        </ul>
+        <h2>Instructions</h2>
+        <p data-testid="instructions">as instrucoes balblalbalblalbla</p>
+        <h2>Video</h2>
+        <iframe
+          width="420"
+          height="315"
+          data-testid="video"
+          src="https://www.youtube.com/embed/tgbNymZ7vqY"
+          title="video da receita"
+        />
+      </div>
+      <div>
+        <h2>Recommended</h2>
+        <ul>
+          <li data-testid={ `${0}-recomendation-card` } />
+          <li data-testid={ `${1}-recomendation-card` } />
+        </ul>
+      </div>
+      <button type="button" data-testid="start-recipe-btn">Start Recipe</button>
+    </div>
+  );
+}
+
+export default RecipeDetails;
---
+++ src/reducers/recipeByIDReducer.js
@@ -0,0 +1,29 @@
+import { REQUEST_RECIPE_BY_ID,
+  REQUEST_RECIPE_BY_ID_SUCCESS,
+  REQUEST_RECIPE_BY_ID_FAILURE } from '../actions/index.actions';
+
+const INICIAL_STATE = {
+  recipe: [],
+  error: '',
+};
+
+const recipeByIDReducer = (state = INICIAL_STATE, action) => {
+  switch (action.type) {
+  case REQUEST_RECIPE_BY_ID:
+    return { ...state };
+  case REQUEST_RECIPE_BY_ID_SUCCESS:
+    return {
+      ...state,
+      recipe: action.recipe,
+    };
+  case REQUEST_RECIPE_BY_ID_FAILURE:
+    return {
+      ...state,
+      error: action.error,
+    };
+  default:
+    return state;
+  }
+};
+
+export default recipeByIDReducer;
