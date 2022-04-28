858a9e31634d96cc404028c2a9832b0d53ddd878
Tamiris Shigaki <shigakiendo@gmail.com>
2022-04-28 15:08:59 -0300

    :heavy_check_mark: feat: requisito 87

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,15 +1,24 @@
-import React from 'react';
-import { useHistory } from 'react-router-dom';
+import React, { useState } from 'react';
+import { useHistory, Redirect } from 'react-router-dom';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
 
 function Profile() {
   const history = useHistory();
+  const [logout, setLogout] = useState(false);
+
+  const handleClearClick = () => {
+    localStorage.clear();
+    setLogout(true);
+  };
+
   return (
     <>
       <Header title="Profile" visible={ false } />
       <div>
-        <h2 data-testid="profile-email"> Email </h2>
+        <h2 data-testid="profile-email">
+          email
+        </h2>
         <button
           type="button"
           data-testid="profile-done-btn"
@@ -27,9 +36,11 @@ function Profile() {
         <button
           type="button"
           data-testid="profile-logout-btn"
+          onClick={ handleClearClick }
         >
           Logout
         </button>
+        { logout && <Redirect to="/" /> }
       </div>
       <Footer />
     </>
