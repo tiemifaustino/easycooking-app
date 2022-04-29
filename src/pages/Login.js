5f3b24b476a20cc5e46efc2a8fd09b978b512041
Tiemi Faustino <hayashipharma@gmail.com>
2022-04-29 17:02:45 -0300

    :lipstick: feat: Estilizaçao CSS da pág Login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { useHistory } from 'react-router-dom';
 import { Form, Button } from 'react-bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
+// import 'bootstrap/dist/css/bootstrap.min.css';
 import './Login.css';
 
 function Login() {
