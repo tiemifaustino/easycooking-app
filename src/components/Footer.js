fcaf8cca5336a8ac3460740c6a4860cd822358a2
Tiemi Faustino <hayashipharma@gmail.com>
2022-05-05 19:08:37 -0300

    :bug: fix: Posicionamento elementos no CSS

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,6 +3,7 @@ import { useHistory } from 'react-router-dom';
 import drinkIcon from '../images/drinkIcon.svg';
 import exploreIcon from '../images/exploreIcon.svg';
 import mealIcon from '../images/mealIcon.svg';
+import './Footer.css';
 
 export default function Footer() {
   const history = useHistory();
