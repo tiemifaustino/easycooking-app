74423b5ef2b2aca7a34b1eff1963ebf1a7e54354
Gabriel Pesch <gkpesch@gmail.com>
2022-05-11 13:21:48 -0300

    fix: Slider now redirects to the page

--- src/components/SimpleSliderRecipes.js
+++ src/components/SimpleSliderRecipes.js
@@ -1,64 +1,71 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Slider from 'react-slick';
 import PropTypes from 'prop-types';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import { Card } from 'react-bootstrap';
+import { useHistory } from 'react-router-dom';
 
-class SimpleSliderRecipes extends Component {
-  render() {
-    const settings = {
-      dots: true,
-      infinite: false,
-      speed: 500,
-      slidesToShow: 2,
-      slidesToScroll: 1,
-    };
-    const { recommendedCards } = this.props;
-    return (
-      <div className="margin-carousel p-1">
-        <Slider { ...settings }>
-          {
-            recommendedCards && recommendedCards.map((card, index) => (
-              <div key={ card.idMeal }>
-                <Card
-                  key={ card.idMeal }
-                  data-testid={ `${index}-recomendation-card` }
-                  style={ { width: '8.5rem' } }
-                  className="card-title card-carousel"
+function SimpleSliderRecipes({ recommendedCards }) {
+  const history = useHistory();
+
+  const settings = {
+    dots: true,
+    infinite: false,
+    speed: 500,
+    slidesToShow: 2,
+    slidesToScroll: 1,
+  };
+
+  const handleClick = (id) => {
+    history.push(`/foods/${id}`);
+  };
+
+  return (
+    <div className="margin-carousel p-1">
+      <Slider { ...settings }>
+        {
+          recommendedCards && recommendedCards.map((card, index) => (
+            <div key={ card.idMeal }>
+              <Card
+                key={ card.idMeal }
+                data-testid={ `${index}-recomendation-card` }
+                style={ { width: '8.5rem' } }
+                className="card-title card-carousel"
+                role="button"
+                onClick={ () => handleClick(card.idMeal) }
+              >
+                <Card.Img
+                  src={ card.strMealThumb }
+                  alt={ `${card.strMeal} thumbnail` }
+                  variant="top"
+                  className="card-carousel-img"
+                />
+                <Card.Text
+                  className="title title-align container-page"
                 >
-                  <Card.Img
-                    src={ card.strMealThumb }
-                    alt={ `${card.strMeal} thumbnail` }
-                    variant="top"
-                    className="card-carousel-img"
-                  />
-                  <Card.Text
-                    className="title title-align container-page"
-                  >
-                    {card.strCategory}
+                  {card.strCategory}
 
-                  </Card.Text>
-                  <Card.Title
-                    data-testid={ `${index}-recomendation-title` }
-                    className="px-1 title-align"
-                  >
-                    {card.strMeal}
+                </Card.Text>
+                <Card.Title
+                  data-testid={ `${index}-recomendation-title` }
+                  className="px-1 title-align"
+                >
+                  {card.strMeal}
 
-                  </Card.Title
-                  >
-                </Card>
-              </div>
-            ))
-          }
-        </Slider>
-      </div>
-    );
-  }
+                </Card.Title
+                >
+              </Card>
+            </div>
+          ))
+        }
+      </Slider>
+    </div>
+  );
 }
 
 SimpleSliderRecipes.propTypes = {
-  recommendedCards: PropTypes.any,
+  recommendedCards: PropTypes.instanceOf(Array),
 }.isRequired;
 
 export default SimpleSliderRecipes;
